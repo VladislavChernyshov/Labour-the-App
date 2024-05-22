1226f9eb0affc1148cab1876758194a26a2f3d1f
FusionTeamQA <qatestfusion@gmail.com>
2024-05-22 09:55:05 +0300

    Initial Commit

---
+++ Labour the App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Labour the App
+//
+//  Created by Vladislav Chernyshov on 22.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Labour the App/Labour_the_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Labour_the_AppApp.swift
+//  Labour the App
+//
+//  Created by Vladislav Chernyshov on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Labour_the_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
